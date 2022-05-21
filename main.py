df8a7c790676b5c751633afbc2478e7ce51688b9
Gary Ditsch <garyditsch@gmail.com>
2022-05-21 07:25:41 -0400

    adding some functions to say hi

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+# printing hello to the terminal
 print('hello world')
 
+# defining a function to print hello
 def sayingHello(): 
     print('hi from function')
 
+# calling the function
 sayingHello()
+
+# create variable
+message = 'Say hi as a variable'
+
+# printing the variable
+print('my variable: ', message)
+
+# defining function that accepts arguments
+def sayHiWithArg(hello):
+    print('This is my argument: ', hello)
+
+# calling the function
+sayHiWithArg(message)
