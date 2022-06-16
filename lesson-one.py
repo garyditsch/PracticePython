2a3bb90a8fc1bf9762fbcb98fc9abbd8ac23ba68
Gary Ditsch <garyditsch@gmail.com>
2022-06-16 18:09:51 -0400

    updated lesson one

--- lesson-one.py
+++ lesson-one.py
@@ -31,6 +31,7 @@ print(string_2)
 # .lower()
 ## here are a few examples which we will use after creating a string
 
+
 super_hero1 = 'spiderman'
 super_hero2 = 'The hulk'
 
@@ -44,19 +45,22 @@ print(super_hero2.title())
 print(super_hero2.upper())
 print(super_hero2.lower())
 
-# Using variable within a string
-# This requires using the format method which is f
+# Using a variable within a string
+# This requires using an f-string (or formatted string literals)
 
-first_name = 'Tony'
-last_name = 'Stark'
-full_name = f"{first_name} {last_name}"
+# Lets create our string variables
+super_hero1 = 'spiderman'
+super_hero2 = 'The hulk'
+first_name = 'tony'
+last_name = 'stark'
 
-print(full_name)
+# Here I am going to just create a new variable using first and last name
+# I am also making sure the names are capitalized using the title method
+full_name = first_name + ' ' + last_name
+print(full_name.title())
 
 # adding some of these things together
-
-favorite_heros = f"I like {super_hero2.title()} and {full_name}, but my favorite is {super_hero1.title()}."
-
+favorite_heros = f"I like {super_hero2.title()}, but my favorite is {super_hero1.title()}."
 print(favorite_heros)
 
 # dealing with white space in a string
